d8c5e02e774b27749de23cb2d44aa74981036015
artemihx <art.pozx1@gmail.com>
2024-12-01 13:53:01 +0700

    header api (by oldber1)

--- project/backend/routes/api.php
+++ project/backend/routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HeaderController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -15,4 +16,9 @@ Route::group(['middleware' => 'auth:sanctum'], function (){
     Route::get('/logout', [AuthController::class, 'logout']);
 });
 
-Route::get('/asd', function(){ return 'asd';});
+
+// данные шапки
+Route::get('/header', [HeaderController::class, 'index']);
+
+// Обновление (авторизованные админы)
+Route::middleware('auth:sanctum')->put('/header', [HeaderController::class, 'update']);
