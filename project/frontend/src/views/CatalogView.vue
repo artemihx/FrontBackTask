f8dce2bfab979ca6ba4888c4deaa7cdeca7baf56
staskachaev <staskachaev205@mail.ru>
2024-12-08 21:22:22 +0700

    add skeleton rooms for catalog

--- project/frontend/src/views/CatalogView.vue
+++ project/frontend/src/views/CatalogView.vue
@@ -4,6 +4,7 @@ import CatalogFilters from "@/components/catalog/CatalogFilters.vue";
 import CatalogSorting from "@/components/catalog/СatalogSorting.vue"
 import { onMounted } from "vue";
 import { useCatalogStore } from "@/stores/CatalogStore.js";
+import RoomCatalogSkeleton from "@/components/room/RoomCatalogSkeleton.vue";
 
 const catalogStore = useCatalogStore();
 const { getRooms } = useCatalogStore();
@@ -18,15 +19,27 @@ onMounted(async () => {
   <div class="flex justify-between">
     <div class="flex flex-col">
       <catalog-sorting/>
-      <section
-        class="rooms"
-      >
-        <room-catalog-card
-          v-for="room in catalogStore.rooms"
-          :key="room.id"
-          :room="room"
-        />
-      </section>
+      <template v-if="catalogStore.rooms.length">
+        <section
+          class="rooms"
+        >
+          <room-catalog-card
+            v-for="room in catalogStore.rooms"
+            :key="room.id"
+            :room="room"
+          />
+        </section>
+      </template>
+      <template v-else>
+        <section
+          class="rooms"
+        >
+          <room-catalog-skeleton
+            v-for="item in 6"
+            :key="item"
+          />
+        </section>
+      </template>
     </div>
     <catalog-filters/>
   </div>
