9be838332705382dc15375e2fa6a24188002065a
staskachaev <staskachaev205@mail.ru>
2024-12-09 12:35:05 +0700

    fix

--- project/frontend/src/views/index.js
+++
@@ -1,7 +0,0 @@
-import HomeView  from "@/views/HomeView.vue";
-import NotFoundView from "@/views/NotFoundView.vue";
-import RegisterView from "@/views/RegisterView.vue";
-import CatalogView from "@/views/CatalogView.vue";
-import ProfileView from "@/views/ProfileView.vue";
-
-export { HomeView, NotFoundView, RegisterView, CatalogView, ProfileView }
