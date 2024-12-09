69db47893416ca601eee0c7fdf9af4c7663627bb
oldber1 <papasa863@gmail.com>
2024-12-09 15:56:09 +0300

    норм контакты

--- project/backend/routes/api.php
+++ project/backend/routes/api.php
@@ -10,6 +10,7 @@ use App\Http\Controllers\Admin\UserController;
 use App\Http\Controllers\Admin\RoomFeatureController;
 use App\Http\Controllers\BookingController;
 use App\Http\Controllers\ContactController;
+//
 
 // Авторизация и регистрация
 Route::post('/register', [AuthController::class, 'register']);
